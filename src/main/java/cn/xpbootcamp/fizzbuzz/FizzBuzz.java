d340c8e3d6452bb66e13c4bee4e4895d1b25a461
Your Name <you@example.com>
2020-05-24 17:19:08 +0800

    add unit test should_return_Fizz_when_number_is_3

--- src/main/java/cn/xpbootcamp/fizzbuzz/FizzBuzz.java
+++ src/main/java/cn/xpbootcamp/fizzbuzz/FizzBuzz.java
@@ -2,6 +2,8 @@ package cn.xpbootcamp.fizzbuzz;
 
 public class FizzBuzz {
     public String say(int i) {
+        if (i%3==0)
+            return "Fizz";
         return "1";
     }
 }
--- src/test/java/cn/xpbootcamp/fizzbuzz/TestFizzBuzz.java
+++ src/test/java/cn/xpbootcamp/fizzbuzz/TestFizzBuzz.java
@@ -11,4 +11,9 @@ public class TestFizzBuzz {
     public void should_return_1_when_number_is_1(){
         assertThat(fizzBuzz.say(1)).isEqualTo("1");
     }
+
+    @Test
+    public void should_return_Fizz_when_number_is_3(){
+        assertThat(fizzBuzz.say(3)).isEqualTo("Fizz");
+    }
 }
